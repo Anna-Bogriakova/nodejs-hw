84257a3287349ce22cb3e355c4d48e90ef738f57
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-09-24 20:22:01 +0200

    welcome

--- src/server.js
+++ src/server.js
@@ -53,3 +53,7 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my API 🚀" });
+});
