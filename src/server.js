a7c99d27ba66ceb3d7f9410e610c834aa4c57fa6
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-10-03 14:08:13 +0200

    server

--- src/server.js
+++ src/server.js
@@ -1,38 +1,60 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import notesRoutes from "./routes/notesRoutes.js";
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-
-dotenv.config();
+import pino from "pino-http";
+import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT ?? 3030;
 
-// middleware
-app.use(cors());
 app.use(express.json());
-app.use(logger);
+app.use(cors());
+app.use(
+  pino({
+    level: "info",
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "HH:MM:ss",
+        ignore: "pid,hostname",
+        messageFormat:
+          "{req.method} {req.url} {res.statusCode} - {responseTime}ms",
+        hideObject: true,
+      },
+    },
+  })
+);
 
-// routes
-app.use("/notes", notesRoutes);
+// Перший маршрут
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello world!" });
+});
 
-// 404 handler
-app.use(notFoundHandler);
+app.get("/notes", (req, res) => {
+  res.status(200).json({ message: "Retrieved all notes" });
+});
 
-// error handler
-app.use(errorHandler);
+app.get("/notes/:noteId", (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
 
-// старт сервера після з’єднання з MongoDB
-const startServer = async () => {
-  await connectMongoDB();
-  app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
+app.get("/test-error", () => {
+  throw new Error("Simulated server error");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error:", err.message);
+  res.status(500).json({
+    message: err.message,
   });
-};
+});
 
-startServer();
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
