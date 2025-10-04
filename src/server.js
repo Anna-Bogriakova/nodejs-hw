970ab0170bd820358ad337b0fae68307ce297a76
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-10-04 14:30:26 +0200

    server fixed

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { connectMongoDB } from "./db/connectMongoDB.js";
-import { notesRouter } from "./routes/notesRoutes.js";
+import notesRouter from "./routes/notesRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
