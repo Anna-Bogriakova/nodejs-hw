afca77e3089ecc21e58d5cbed3fbb59adfd91014
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-10-21 12:53:55 +0200

    authValidation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,20 +1,21 @@
+import { celebrate } from "celebrate";
 import { Router } from "express";
-import {
-  registerUser,
-  loginUser,
-  refreshUserSession,
-  logoutUser,
-} from "../controllers/authController.js";
 import {
   registerUserSchema,
   loginUserSchema,
 } from "../validations/authValidation.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+} from "../controllers/authController.js";
 
 const router = Router();
 
-router.post("/auth/register", registerUserSchema, registerUser);
-router.post("/auth/login", loginUserSchema, loginUser);
-router.post("/auth/refresh", refreshUserSession); // no body; cookies used
-router.post("/auth/logout", logoutUser); // no body; cookies used
+router.post("/auth/register", celebrate(registerUserSchema), registerUser);
+router.post("/auth/login", celebrate(loginUserSchema), loginUser);
+router.post("/auth/refresh", refreshUserSession);
+router.post("/auth/logout", logoutUser);
 
 export default router;
