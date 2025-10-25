f4eb52800101e0a085507719cedaf91498ddb881
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-10-25 15:01:45 +0200

    validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -3,17 +3,17 @@ import { isValidObjectId } from "mongoose";
 import { TAGS } from "../constants/tags.js";
 
 // Валідація для GET /notes
-export const getAllNotesSchema = celebrate({
+export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(""),
   }),
-});
+};
 
 // Валідація для :noteId
-export const noteIdSchema = celebrate({
+export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom((value, helpers) => {
       if (!isValidObjectId(value)) {
@@ -22,19 +22,19 @@ export const noteIdSchema = celebrate({
       return value;
     }, "ObjectId validation"),
   }),
-});
+};
 
 // POST /notes
-export const createNoteSchema = celebrate({
+export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(""),
     tag: Joi.string().valid(...TAGS),
   }),
-});
+};
 
 // PATCH /notes/:noteId
-export const updateNoteSchema = celebrate({
+export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom((value, helpers) => {
       if (!isValidObjectId(value)) {
@@ -48,9 +48,9 @@ export const updateNoteSchema = celebrate({
     content: Joi.string().allow(""),
     tag: Joi.string().valid(...TAGS),
   })
-    .min(1) // ✅ вимагає наявність хоча б одного поля
+    .min(1)
     .messages({
       "object.min":
         "At least one field (title, content, or tag) must be provided",
     }),
-});
+};
